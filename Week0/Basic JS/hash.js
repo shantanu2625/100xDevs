8b664fb8d36c7428383f16b138a5547e218a3b11
shantanu2625 <143947633+shantanu2625@users.noreply.github.com>
2024-08-16 12:46:07 +0530

    Changed

--- Week0/Basic JS/hash.js
+++ Week0/Basic JS/hash.js
@@ -1,21 +1,19 @@
 const crypto = require('crypto');
 
-// Write a function to find a input string that produce a hash which starts with "00000" 
-
+// Function to find an input string that produces a hash starting with '00000'
 function findHashWithPrefix(prefix) {
+    let input = 0;
     while (true) {
-        let input = 0;
-        let inputStr = input.toString();
+        let inputStr = "Shan and Ish" + input.toString();
         let hash = crypto.createHash('sha256').update(inputStr).digest('hex');
         if (hash.startsWith(prefix)) {
-            return {input : inputStr, hash : hash};
+            return { input: inputStr, hash: hash };
         }
-    input++;
+        input++;
+    }
+}
 
-    }     
-
-} 
-    //find and print the input string and hash
+// Find and print the input string and hash
 const result = findHashWithPrefix('00000');
-console.log('Input: ${result.input}');
-console.log('Hash: ${result.hash}');
+console.log(`Input: ${result.input}`);
+console.log(`Hash: ${result.hash}`);
