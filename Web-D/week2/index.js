5354fa2be6d870a3b3682778a11664cd3f0b83c0
shantanu2625 <143947633+shantanu2625@users.noreply.github.com>
2024-08-21 18:07:11 +0530

    AU 21

--- Web-D/week2/index.js
+++ Web-D/week2/index.js
@@ -1,8 +1,21 @@
+function fetchData(callBack) {
+    setTimeout(() => {
+        if (Math.random() < 0.5) {
+            callBack(null, "data fetched succesfully!");
+        } else {
+            callBack(new Error("failed to fetched data"));
+        
+            
+        }
+    }, 1000)
+} 
 
-console.log("Hi!");
+fetchData((err,data) => {
+    if (err) {
+        console.log("Error: ", err.message);
+    } else {
+        console.log("Data: ", data);
+    }
 
-setTimeout(function timeout() {
-    console.log("Click the button!");
-}, 15000);
+});
 
-console.log("Welcome to loupe."); 
