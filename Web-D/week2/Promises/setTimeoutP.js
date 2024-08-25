901ddaaf5a0f769fb5056b6edb869c09dbc1992e
shantanu2625 <143947633+shantanu2625@users.noreply.github.com>
2024-08-25 09:25:18 +0530

    AU 25

--- Web-D/week2/Promises/setTimeoutP.js
+++ Web-D/week2/Promises/setTimeoutP.js
@@ -1,1 +1,9 @@
-function SetTimeoutPromisified(delay)
+function SetTimeoutPromisified(ms) {
+    return new Promise(resolve => setTimeout(resolve,ms))
+} 
+
+function callback() {
+    console.log("hi there 3 seconds have passed!")
+} 
+
+SetTimeoutPromisified(5000).then(callback);
