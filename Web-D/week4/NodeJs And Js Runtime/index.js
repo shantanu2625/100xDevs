9907892ad82b861ac5b23d2f5fc53ffd555ad2b7
shantanu2625 <143947633+shantanu2625@users.noreply.github.com>
2024-10-04 18:35:27 +0530

    OCT 4

--- Web-D/week4/NodeJs And Js Runtime/index.js
+++ Web-D/week4/NodeJs And Js Runtime/index.js
@@ -1,2 +1,25 @@
 const fs = require('fs');
-const path = require('path');
+const { Command } = require('commander');
+
+const program = new Command();
+
+program
+    .name('cli-args-counter')
+    .description('writing a cli to do some string utilites like counting the no. of words in a passed argument')
+    .version('0.8.0'); 
+
+program.command('counter')
+    .description("count number of lines a file")
+    .argument('<file>','file to count')
+    .action((file)=> {
+        fs.readFile(file,'utf-8',(err, data) => {
+            if (err) {
+                console.log(err)
+            } else {
+                const lines = data.split('\n').length;
+            } 
+            console.log(`There are ${lines} lines in ${file} `)
+        });
+    });
+     
+program.parse
