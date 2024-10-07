4bd93129a831d58ef8681ad6f76462ec1ccd9a85
shantanu2625 <143947633+shantanu2625@users.noreply.github.com>
2024-10-07 12:25:39 +0530

    OCT 7

--- Web-D/week4/NodeJs And Js Runtime/index.js
+++ Web-D/week4/NodeJs And Js Runtime/index.js
@@ -1,6 +1,22 @@
+
+
 const fs = require('fs');
 const { Command } = require('commander');
 const program = new Command();
 
 program
     .name("counter")
+    .description("CLI to do file-based tasks")
+    .argument('<file>', 'file to count')
+    .action((file) => {
+        fs.readFile(file, 'utf-8', (err, data) => {
+            if (err) {
+                console.error("Error reading file:", err);
+            } else { 
+                const lines = data.split('\n').length;
+                console.log(`There are ${lines} lines in this ${file} file`);
+            }
+        });
+    });
+
+program.parse(process.argv); // Ensure process.argv is passed
