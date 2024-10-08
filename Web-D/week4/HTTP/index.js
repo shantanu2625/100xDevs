7edbdd08edcb0c92268c9f848f9112fe51b6bfab
shantanu2625 <143947633+shantanu2625@users.noreply.github.com>
2024-10-08 17:39:04 +0530

    OCT 8

--- Web-D/week4/HTTP/index.js
+++ Web-D/week4/HTTP/index.js
@@ -1,8 +1,28 @@
+// const fs = require('fs');
+
+// fs.readFile( file, 'utf-8', (err,data)=> {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log(data)
+//     }
+// } )
+
+
 const express = require('express');
 const app = express()
 
 app.get('/' , function (req, res){
-    res.send("Hello Shan");
+    res.send("Hello Shan <h1>This is my first http server </h1>");
+    res.send("<h1>Hello World</h1>")
 }) 
+
+app.post('/' , function(req,res){
+    res.send("Hello from the post endpoint");
+}) 
+
+app.get('/asd',function(req, res){
+    res.send("Hello from asd endpoint")
+})
 
 app.listen(3000);
