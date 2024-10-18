31c7dbdc32fc89fca6f832b555893a910bcd99ce
shantanu2625 <143947633+shantanu2625@users.noreply.github.com>
2024-10-18 15:43:18 +0530

    --help

--- Web-D/week4/NodeJs And Js Runtime/index3.js
+++ Web-D/week4/NodeJs And Js Runtime/index3.js
@@ -7,6 +7,6 @@ fs.readFile(filePath, 'utf-8', (err, data) => {
     if(err) {
         console.log(err);
     } else {
-        console.log(data)
+        console.log(data);
     }
 });
--- Web-D/week5/Headers,Query Params And Express/http-server/index0.js
+++ Web-D/week5/Headers,Query Params And Express/http-server/index0.js
@@ -4,10 +4,12 @@
 const express = require("express");
 const app = express();
 
-app.use(function middleware(req, res, next) {
+function middleware(req, res, next) {
     console.log(`The request method is: ${req.method} ${req.hostname} ${new Date()} `) 
     next();
-})
+} 
+
+app.use(middleware);
 
 
 app.get('/sum', function(req, res){
@@ -17,7 +19,11 @@ app.get('/sum', function(req, res){
     res.json({
         ans: a + b
     })
-})
+}) 
+
+
+
+
 app.get('/multiply', function(req, res){
     const a = req.query.a;
     const b = req.query.b;
