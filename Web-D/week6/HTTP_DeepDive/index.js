22f50814a2ecaa3f5087edc43099227188cb40b8
shantanu2625 <143947633+shantanu2625@users.noreply.github.com>
2024-10-19 13:44:52 +0530

    OCT 19

--- Web-D/week6/HTTP_DeepDive/index.js
+++ Web-D/week6/HTTP_DeepDive/index.js
@@ -1,1 +1,26 @@
-const express 
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const users = [];
+
+app.post('/signup', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    users.push({
+        username,
+        password
+    }) 
+
+    res.send({
+        message: "You have signed up"
+    })
+}) 
+
+app.post('/signin', (req, res) => {
+
+}) 
+
+app.listen(3000);
