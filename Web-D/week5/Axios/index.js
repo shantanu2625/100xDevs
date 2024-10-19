184b6880713220cb9b6bf9cb466f3e1c5b8f0328
shantanu2625 <143947633+shantanu2625@users.noreply.github.com>
2024-10-19 10:34:37 +0530

    OCT 19

---
+++ Web-D/week5/Axios/index.js
@@ -0,0 +1,19 @@
+const axios = require('axios');
+
+// function main() {
+//     fetch("https://sum-server.100xdevs.com/todos") 
+//         .then (async (response) => {
+//             const json = await response.json();
+//             console.log(json.todos.length)
+//         });
+// };
+
+
+
+async function main() {
+    const response = await fetch("https://sum-server.100xdevs.com/todos");
+    const json = await response.json;
+    console.log(json.todos.length);
+} 
+
+main();
