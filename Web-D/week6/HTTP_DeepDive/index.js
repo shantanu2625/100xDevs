b081c1ceaad09aee137db612236e39a7a04bc346
shantanu2625 <143947633+shantanu2625@users.noreply.github.com>
2024-10-21 11:43:49 +0530

    OCT 21

--- Web-D/week6/HTTP_DeepDive/index.js
+++ Web-D/week6/HTTP_DeepDive/index.js
@@ -5,19 +5,7 @@ app.use(express.json());
 
 const users = []; 
 
-function generateToken() {
-    let options  = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-
-    let token = "";
-
-    for (let i = 0; i < 32; i++) {
-        token += options[Math.floor(Math.random() * options.length)];
-
-    } 
-
-    return token;
-
-}
+const JWT_SECRET = "USER_APP";
 
 app.post('/signup', (req, res) => {
     const username = req.body.username;
