e290e5efc57bcb8417ffdbce749d8fe750b641f7
shantanu2625 <143947633+shantanu2625@users.noreply.github.com>
2024-10-28 11:29:47 +0530

    OCT 28

---
+++ Web-D/week7/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.post('/signup', function(req, res){
+
+})
+
+app.post('/signin', function(req, res){
+
+})
+app.post('/todo', function(req, res){
+
+})
+app.post('/signin', function(req, res){
+ 
+}) 
+
+app.listen(3000);
+ 
+
