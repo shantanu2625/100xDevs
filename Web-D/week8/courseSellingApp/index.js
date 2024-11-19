413199a87ba9e8acf9159f610b7e9a8f6f168e94
shantanu2625 <143947633+shantanu2625@users.noreply.github.com>
2024-11-19 12:30:35 +0530

    NOV 19

--- Web-D/week8/courseSellingApp/index.js
+++ Web-D/week8/courseSellingApp/index.js
@@ -1,7 +1,16 @@
 const jwt = require('jsonwebtoken');
-const express = require('express');
+const express = require('express'); 
+const mongoose = require('mongoose'); 
+const bcrypt = require('bcrypt');
 const app = express();
 
 app.post('/login', (req, res)=>{
 
-})
+}) 
+
+app.post('/signup', (req, res)=>{
+    
+} )
+
+
+app.listen(3000);
