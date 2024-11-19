9c6e262b82a27a25cc47f3bd3e0ce25da30798ea
shantanu2625 <143947633+shantanu2625@users.noreply.github.com>
2024-11-19 17:51:31 +0530

    NOV 19

--- Web-D/week8/courseSellingApp/db.js
+++ Web-D/week8/courseSellingApp/db.js
@@ -1,12 +1,12 @@
-const { Schema, default: mongoose } = require("mongoose");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Types.ObjectId;
 
 const userSchema = Schema({
     email: {type: String, unique: true},
     password: String,
     firstName: String,
     lastName: String
-
-
 }) 
 
 const adminSchema = Schema({
@@ -25,10 +25,19 @@ const courseSchema = Schema({
 }) 
 
 const purchaseSchema = Schema({
+    userId: ObjectId,
+    courseId: ObjectId,
 
 }) 
 
 const userModel = mongoose.model("user", userSchema);
 const adminModel = mongoose.model("admin", userSchema);
 const courseModel = mongoose.model("course", userSchema);
-const purchaseModel = mongoose.model("purchase", userSchema);
+const purchaseModel = mongoose.model("purchase", userSchema); 
+
+module.exports = {
+    userModel,
+    adminModel,
+    courseModel,
+    purchaseModel
+}
