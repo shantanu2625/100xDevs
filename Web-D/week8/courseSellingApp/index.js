6666444c9af2d483584c7b00f66c154a6131af64
shantanu2625 <143947633+shantanu2625@users.noreply.github.com>
2024-11-19 13:08:46 +0530

    NOV 19

--- Web-D/week8/courseSellingApp/index.js
+++ Web-D/week8/courseSellingApp/index.js
@@ -4,23 +4,7 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const app = express();
 
-app.post('/user/signup', (req, res)=>{
-    res.json({
-        message: "Signup Endpoint"
-    })
-}) 
-
-app.post('/user/signin', function(req, res){
-    res.json({
-        message: "Signin Endpoint"
-    })
-})   
-
-app.get('/user/purchases', function(req, res){
-    res.json({
-        message: "Purchases Endpoint"
-    })
-}) 
+ 
 
 app.post('/course/purchase', function(req, res){
     res.json({
