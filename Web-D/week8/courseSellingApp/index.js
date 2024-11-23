094331b3ae1500602ce0ac0cb093dfcf25f8e922
shantanu2625 <143947633+shantanu2625@users.noreply.github.com>
2024-11-23 20:35:44 +0530

    NOV 19

--- Web-D/week8/courseSellingApp/index.js
+++ Web-D/week8/courseSellingApp/index.js
@@ -6,6 +6,7 @@ const { courseRouter } = require("./routes/course");
 const { adminRouter } = require("./routes/admin");
 const app = express();
 
+
 app.use('/api/v1/user', userRouter);
 app.use('/api/v1/admin', adminRouter)
 app.use('/api/v1/course', courseRouter);
