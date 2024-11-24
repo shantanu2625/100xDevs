692964dbc3e8accb8cc6d2500e2ff9dee00bb048
shantanu2625 <143947633+shantanu2625@users.noreply.github.com>
2024-11-24 18:45:22 +0530

    NOV 24

--- Web-D/week8/courseSellingApp/routes/user.js
+++ Web-D/week8/courseSellingApp/routes/user.js
@@ -1,11 +1,28 @@
 const { Router } = require("express");
+const { userModel } = require("../db")
 const userRouter = Router();
+const { z } = require("zod");
 
 
-userRouter.post('/signup', (req, res)=>{
-    const {email, password, firstName, lastName} = req.body;
+userRouter.post('/signup', async (req, res)=>{
+    const requiredBody = z.object({
+        email: z.string().min(10).max(40).email(),
+        password: z.string().min(6).max(20),
+        firstName: z.string().min(2).max(15),
+        lastName: z.string().min(5).max(10)
+    }) 
+    try {
+    await userModel.create({
+        email: email,
+        password: password,
+        firstName,
+        lastName
+    }) 
+} catch (e) {
+    
+}
     res.json({
-        message: "Signup Endpoint"
+        message: "Signup Succeeded"
     })
 }) 
 
