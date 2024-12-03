f6162f10849777994e733eddde0028875ea48974
shantanu2625 <143947633+shantanu2625@users.noreply.github.com>
2024-12-03 16:01:18 +0530

    DEC03

--- Web-D/week8/courseSellingApp/index.js
+++ Web-D/week8/courseSellingApp/index.js
@@ -5,6 +5,7 @@ const { userRouter } = require("./routes/user");
 const { courseRouter } = require("./routes/course");
 const { adminRouter } = require("./routes/admin");
 const app = express();
+app.use(express.json())
 
 
 app.use('/api/v1/user', userRouter);
@@ -13,7 +14,7 @@ app.use('/api/v1/course', courseRouter);
 
 
 async function main() {
-    await mongoose.connect("mongodb+srv://shantanubangar1:BsqkgdARfhc39WhS@cluster0.wi1xv.mongodb.net/coursera-app");
+    await mongoose.connection("mongodb+srv://shantanubangar1:BsqkgdARfhc39WhS@cluster0.wi1xv.mongodb.net/coursera-app");
     app.listen(3000);
     console.log("Listening on port 3000")
 }
