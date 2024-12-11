b2a617a8ffe5417c5be2f1ba35b2fb6e2babf30a
shantanu2625 <143947633+shantanu2625@users.noreply.github.com>
2024-12-11 18:33:13 +0530

    DEC11

--- Web-D/week8/courseSellingApp/middleware/user.js
+++ Web-D/week8/courseSellingApp/middleware/user.js
@@ -3,7 +3,7 @@ const {JWT_USER_PASSWORD} = require("../config");
 
 function userMiddleware(req, res, next) {
     const token = req.headers.token;
-    const decoded = jwt.verify(token, ); 
+    const decoded = jwt.verify(token, JWT_USER_PASSWORD); 
 
     if (decoded) {
         req.userId = decoded.id;
