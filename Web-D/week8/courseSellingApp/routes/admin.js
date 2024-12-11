3b903f86bb2f659130f4290009b5b932be74aa67
shantanu2625 <143947633+shantanu2625@users.noreply.github.com>
2024-12-11 14:25:08 +0530

    DEC 11

--- Web-D/week8/courseSellingApp/routes/admin.js
+++ Web-D/week8/courseSellingApp/routes/admin.js
@@ -19,10 +19,27 @@ adminRouter.post("/signup", async function(req, res){
     })
 }) 
 
-adminRouter.post("/signin", function(req, res){
-    res.json({
-        message: "Signin Endpoint"
-    })
+adminRouter.post("/signin", async function(req, res){
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({
+        email: email,
+        password: password
+    });
+
+    if (user) {
+        const token  = jwt.sign({
+            id: user._id
+        }, JWT_USER_PASSWORD); 
+
+        res.json({
+          token: token
+        })
+    } else {
+        res.status(403).json({
+            message: "Incorrect credentials"
+        })
+    }
 }) 
 
 adminRouter.post("/course", function(req, res){
