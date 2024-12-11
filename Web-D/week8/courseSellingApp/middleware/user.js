af0d65c12c392208f2cbbfe39db1d637229a26aa
shantanu2625 <143947633+shantanu2625@users.noreply.github.com>
2024-12-11 18:23:17 +0530

    DEC11

--- Web-D/week8/courseSellingApp/middleware/user.js
+++ Web-D/week8/courseSellingApp/middleware/user.js
@@ -5,6 +5,15 @@ function userMiddleware(req, res, next) {
     const token = req.headers.token;
     const decoded = jwt.verify(token, ); 
 
+    if (decoded) {
+        req.userId = decoded.id;
+        next();
+    } else {
+        res.status(403).json({
+            message: "You are not signed in"
+        })
+    }
+
 } 
 
 module.exports = {
